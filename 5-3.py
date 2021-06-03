1fea31cc3367218403985b453e484d79044feff7
Jeff Chiu <jwchiu82@gmail.com>
2021-06-02 20:55:45 -0700

    finish 5-3 exercise

---
+++ 5-3.py
@@ -0,0 +1,4 @@
+sum = 0
+for n in range (2, 101, 2):
+  sum += n
+print(sum)
--- day-5-practice.py
+++ day-5-practice.py
@@ -1,4 +1,8 @@
-fruits = ["Apple", "Peach", "Pear"]
-for fruit in fruits:
-  print(fruit)
-  print(fruit + " Pie")
+# fruits = ["Apple", "Peach", "Pear"]
+# for fruit in fruits:
+#   print(fruit)
+#   print(fruit + " Pie")
+
+total = 0
+for n in range(1, 101):
+  total += n
